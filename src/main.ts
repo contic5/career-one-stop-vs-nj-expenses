4272fa3097b4770f906ca981461661291e1a42b4
Christopher Joseph Conti <36709920+contic5@users.noreply.github.com>
2026-07-29 14:52:09 -0400

    Added appartment data and determined next steps.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ async function main()
 {
   career_data=await get_data("CareerOneStop_Data.xlsx");
   console.log(career_data);
+
+  appartment_data=await get_data("New_Jersey_Expenses.xlsx","Appartments");
+  console.log(appartment_data);
 }
 
 let career_data=[];
+let appartment_data=[];
 main();
